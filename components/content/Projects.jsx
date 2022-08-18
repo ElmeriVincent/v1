c0e1d6c7f22d6c39fb767d7b19424549e4c2f040
Elmeri Keitaanranta <ElmeriVincent@gmail.com>
2022-08-18 21:24:39 +0300

    Added Buukbox To Projects

--- components/content/Projects.jsx
+++ components/content/Projects.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Buukbox from './projects/Buukbox'
 
 export default function Projects() {
     return (
         <section>
             <section id="projects-section">
-                <div className="flex justify-center h-screen items-center text-white">Projects</div>
+                <div className="flex justify-center items-center flex-col h-screen">
+                    <h1 className="font-NTR text-3xl border-nordfrostlight border-b border-l px-2 text-white text-right capitalize">Most recent projects</h1>
+                    <div className="grid xl:grid-cols-3 md:grid-cols-1 lg:grid-cols-3">
+                        <Buukbox />
+                        <Buukbox />
+                        <Buukbox />
+                    </div>
+                </div>
             </section>
         </section>
     )
