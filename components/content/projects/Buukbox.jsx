a5b64d846a45883b8fc4f8abb9874169f141cf1a
Elmeri Keitaanranta <ElmeriVincent@gmail.com>
2022-08-19 22:35:04 +0300

    Update BuukBox Display

--- components/content/projects/Buukbox.jsx
+++ components/content/projects/Buukbox.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
 
 import { DiReact } from 'react-icons/di';
 import { SiTailwindcss } from 'react-icons/si'
 
+import buukboxCover from '../../../public/images/buukboxCover.png'
 
 export default function Buukbox() {
     return (
         <div className="buukbox-card">
             <div className="mt-8 mx-4">
-                <div className="border-2 rounded border-nordaurorapink bg-nordpolarevening p-2">
+                <div className="border rounded border-transparent bg-nordpolarevening p-2">
                     <div className="card-content">
-                        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdesignshack.net%2Fwp-content%2Fuploads%2Fplaceholder-image.png&f=1&nofb=1"
-                            className="object-cover w-80 h-full"
-                            alt="..." />
+                        <div className="w-96 rounded cursor-pointer">
+                            <a href={"https://buukbox.vercel.app/"} rel="nooperner noreferrer" target="_blank">
+                                <Image src={buukboxCover}
+                                    className="rounded bg-blend-overlay hover:mix-blend-normal hover:duration-300 opacity-80 hover:opacity-100"
+                                    alt="Buukbox website cover image."
+                                    width={0} height={0} />
+                            </a>
+                        </div>
                         <div className="flex justify-center mt-2 text-nordfrostlight font-NTR text-2xl">
                             <DiReact />
                             <SiTailwindcss />
@@ -20,7 +28,7 @@ export default function Buukbox() {
                             <span className="text-xl font-NTR mx-2">| Vercel</span>
                         </div>
                         <div>
-                            <p className="text-white text-lg font-NTR text-center">Application for searching reading books</p>
+                            <p className="text-white text-lg font-NTR text-center">Application for searching and reading books</p>
                         </div>
                     </div>
                 </div>
