5b3ba9e9de1c0bd2cfcad665fdfbbd985d9108e5
Elmeri Keitaanranta <ElmeriVincent@gmail.com>
2022-08-19 22:30:19 +0300

    Added Game Project

--- components/content/Projects.jsx
+++ components/content/Projects.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+
 import Buukbox from './projects/Buukbox'
+import Authentication from './projects/Authentication'
+import Game from './projects/Game'
 
 export default function Projects() {
     return (
         <section>
-            <section id="projects-section">
+            <section id="projects">
                 <div className="flex justify-center items-center flex-col">
                     <h1 className="font-NTR text-3xl border-nordfrostlight border-b border-l px-2 text-white text-right capitalize mt-24 xl:mt-32">Most recent projects</h1>
-                    <div className="grid xl:grid-cols-3 md:grid-cols-1 lg:grid-cols-3">
+                    <div className="grid xl:grid-cols-3 lg:grid-cols-1 grid-cols-1">
                         <Buukbox />
-                        <Buukbox />
-                        <Buukbox />
+                        <Authentication />
+                        <Game />
                     </div>
                 </div>
             </section>
---
+++ components/content/projects/Game.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Image from 'next/image';
+
+import { TbBrandVisualStudio } from 'react-icons/tb';
+import { FaUnity } from 'react-icons/fa';
+
+import play from '../../../public/images/play.jpg'
+
+export default function Game() {
+    return (
+        <div className="authentication-card">
+            <div className="mt-8 mx-4">
+                <div className="border rounded border-transparent bg-nordpolarevening p-2">
+                    <div className="card-content">
+                        <div className="w-96 rounded cursor-pointer">
+                            <a href={"https://devevk.itch.io/cinderella-mountains"} rel="nooperner noreferrer" target="_blank">
+                                <Image src={play}
+                                    className="rounded bg-blend-overlay hover:mix-blend-normal hover:duration-300 opacity-80 hover:opacity-100"
+                                    alt="Buukbox website cover image."
+                                    width={0} height={0} layout="responsive" />
+                            </a>
+                        </div>
+                        <div className="flex justify-center mt-2 text-nordfrostlight font-NTR text-2xl">
+                            <FaUnity />
+                            <TbBrandVisualStudio />
+                            <span className="text-xl font-NTR mx-2">| Video Game</span>
+                            <span className="text-xl font-NTR mx-2">| C#</span>
+                        </div>
+                        <div>
+                            <p className="text-white text-lg font-NTR text-center">Small Snowboarding Planet Game</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
