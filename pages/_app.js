ea31df4848709f74551590a559aab7cdeaba523e
Elmeri Keitaanranta <ElmeriVincent@gmail.com>
2022-09-07 16:56:12 +0300

    meta

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,10 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>Elmeri Keitaanranta</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Finnish developer who builds things for the web." />
+        <link rel="canonical" href="https://elmerivincent.com/" />
       </Head>
       <Component {...pageProps} />
     </>
